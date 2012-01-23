039b0c4800808bf0b3740eb521d6f0257e3567f5
Pavel <pavel.evst@gmail.com>
2012-01-23 14:50:36 +0800

    tests fix

--- spec/action_view_spec.rb
+++ spec/action_view_spec.rb
@@ -12,12 +12,14 @@ describe "ActionView hacks" do
     str.should =~ %r{link.+href="/stylesheets/application.css.*"}
   end
 
-  it "should work with data- attributes and nested hashes" do
-    str = render_haml('= link_to "An", ?#, data: data_hash', data_hash: {a: 1, b: '2', c: 0.111}).gsub(?', ?")
+  if Rails.version =~ /^3\.[12].*/
+    it "should work with data- attributes and nested hashes" do
+      str = render_haml('= link_to "An", ?#, data: data_hash', data_hash: {a: 1, b: '2', c: 0.111}).gsub(?', ?")
 
-    str.should =~ /data\-a="1"/
-    str.should =~ /data\-b="2"/
-    str.should =~ /data\-c="0.111"/
+      str.should =~ /data\-a="1"/
+      str.should =~ /data\-b="2"/
+      str.should =~ /data\-c="0.111"/
+    end
   end
   
   it "should make itemscope as boolean attribute" do
