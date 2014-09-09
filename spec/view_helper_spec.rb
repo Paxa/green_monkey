51321a8e6089260233a40ea1afb4868e7874eb9f
Pavel <pavel.evst@gmail.com>
2014-09-09 10:22:10 +0700

    Correct haml repo path and remove ruby 1.9.2 from .travis.yml

--- spec/view_helper_spec.rb
+++ spec/view_helper_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe GreenMonkey::ViewHelper do
+  include ActionView::Helpers::TagHelper
   include GreenMonkey::ViewHelper
 
   it "should convert time period to iso8601 period" do
@@ -20,4 +21,22 @@ describe GreenMonkey::ViewHelper do
     time_to_iso8601(1).should == "PT1S"
     time_to_iso8601(0).should == "PT0H0M0S"
   end
+
+  it "should build time tag" do
+    time = DateTime.parse("09-09-2014 9:38AM")
+    time_tag(time).should == %{<time datetime="2014-09-09T09:38:00+00:00">September 09, 2014 09:38</time>}
+  end
+
+  it "should make time interval tag" do
+    time = DateTime.parse("09-09-2014 9:38AM")
+    tag = time_tag_interval(time, 1.hour + 20.minutes)
+    expected_html = %{<time datetime="2014-09-09T09:38:00+00:00/PT1H20M">September 09, 2014 09:38 in 1 hour 20 minutes</time>}
+    tag.should == expected_html
+  end
+
+  it "should build itemscope and itentype attributes" do
+    mida_scope(:Airline).should == %{ itemscope itemtype="http://schema.org/Airline"}
+    mida_scope(Post).should ==     %{ itemscope itemtype="http://schema.org/BlogPosting"}
+  end
+
 end
